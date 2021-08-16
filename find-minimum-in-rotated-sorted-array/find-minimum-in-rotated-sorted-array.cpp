0bc116b27bf7670fe2b23a3385843fe3b364d837
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-16 22:42:22 +0530

    Time: 4 ms (59.41%), Space: 10.3 MB (22.90%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,84 +1,74 @@
-// class Solution {
-    
-    
-//     int binary_search(vector<int>& nums,int left,int right,int target)
-//     {
-//         int mid;
-//         //Iterative binary search
-//         while(left<=right)
-//         {
-//             mid = (left+right)/2;
-//             if(nums[mid]==target)
-//                 return mid;
-//             else if(nums[mid]>target)
-//                 right = mid-1;
-//             else
-//                 left = mid+1;
-//         }
-//         return -1;
-//     }
-// public:
-//     int search(vector<int>& nums, int target) {
-        
-        
-//         if(nums[pivot]==target)
-//             return pivot;
-        
-//         return target>nums[n-1]?binary_search(nums,0,pivot,target):binary_search(nums,pivot+1,n-1,target);
-//     }
-// };
+// BINARY SEARCH
+
+// Identification -
+// Whenever 'sorted' word is used, always think of Binary Search {i.e whenever sorted data is given, we might can
+// improve the complexity using binary search }
 
 
+// Graph of the Rotated Sorted Array
 
-
-
-
-
-
-
-
+//                B /.
+//                 / .
+//                /  .
+//               /   .
+//            A /    .
+//                   .     / D
+//                   .    /
+//                   .   /
+//                   .  /
+//                   . /
+//                 C  /
+//
+//
+//
 
 
 
 class Solution {
-    
-    int find_pivot(vector<int>& nums,int left,int right)
+
+    // Function to return the index of the maximum(pivot) element
+    int find_pivot(vector<int>& nums, int left, int right)
     {
-        if(nums[left] < nums[right])
+        if (nums[left] < nums[right])
             return right;
-        
+
         int mid;
-        //Iterative binary search
-        while(left<right)
+
+        while (left <= right)
         {
-            mid = (left+right)/2;
-            if(nums[mid]>nums[mid+1])
+            mid = left + ((right - left) / 2);
+
+            // both the parts of graph are stricly increasing, so if next element is smaller than the current
+            // element, it can occur only at place B
+            if (nums[mid] > nums[mid + 1])
                 return mid;
-            else if(nums[mid]<nums[0])
-                right = mid;
+
+            // when mid falls in the CD part of the graph
+            else if (nums[mid] < nums[0])
+                right = mid - 1;
+
+            // else mid is in the AB part, so changing left = mid + 1
             else
-                left = mid+1;
+                left = mid + 1;
         }
+
         return mid;
     }
-    
+
 public:
+
+    // Solution starts here
     int findMin(vector<int>& a) {
-        
-        int n=a.size();
-        int start = 0;
-        int end = n - 1;
-        int mid;
-        
-        if(n==1)
+
+        int n = a.size();
+
+        if (n == 1)
             return a[0];
-        
-        int pivot = find_pivot(a,0,n-1);
-        cout<<pivot;
-        
-        return a[(pivot+1)%n];
-        
-        
-        
+
+        // Finding the index of the maximum element of the array
+        int pivot = find_pivot(a, 0, n - 1);
+
+        // The minimum element will be the right adjacent to the maximum(pivot) element
+        return a[(pivot + 1) % n];
     }
 };
