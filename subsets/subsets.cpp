edd85f4e32a8a8482677267cc451930942fd87eb
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-24 23:33:47 +0530

    Time: 4 ms (59.53%), Space: 17 MB (5.29%) - LeetHub

---
+++ subsets/subsets.cpp
@@ -0,0 +1,36 @@
+
+
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    
+    void subsets(vector<int> a,vector<int>out){
+        
+        if(a.size()==0){
+            ans.push_back(out);
+            return;
+        }
+        
+        vector<int> out1 = out;
+        vector<int> out2 = out;
+        
+        out2.push_back(a[0]);
+        
+        a.erase(a.begin());
+        
+        subsets(a,out1);
+        subsets(a,out2);
+        
+    }
+    
+    vector<vector<int>> subsets(vector<int>& a) {
+        
+        vector<int> out;
+        
+        subsets(a,out);
+        
+        return ans;
+        
+    }
+};
