6228c07a39e612a0c74fd346a4004f7a58a7c217
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-09-06 22:11:46 +0530

    Time: 30 ms (13.67%), Space: 21.3 MB (77.21%) - LeetHub

--- convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    TreeNode *sortedArrayToBST(vector<int> &num) {
-        if(num.size() == 0) 
+    TreeNode *SortedtoBST(vector<int> &a,int start,int end){
+        
+        if(start>end)
             return NULL;
         
-        int middle = num.size()/2;
-        TreeNode* root = new TreeNode(num[middle]);
+        int mid = start + ((end-start)/2);
+        TreeNode* root = new TreeNode(a[mid]);
         
-        vector<int> leftInts(num.begin(), num.begin()+middle);
-        vector<int> rightInts(num.begin()+middle+1, num.end());
-        
-        root->left = sortedArrayToBST(leftInts);
-        root->right = sortedArrayToBST(rightInts);
+        root->left = SortedtoBST(a,start,mid-1);
+        root->right = SortedtoBST(a,mid+1,end);
         
         return root;
+    }
+    
+    TreeNode *sortedArrayToBST(vector<int> &a){
+        
+//         if(num.size() == 0) 
+//             return NULL;
+        
+//         int middle = num.size()/2;
+//         TreeNode* root = new TreeNode(num[middle]);
+        
+//         vector<int> leftInts(num.begin(), num.begin()+middle);
+//         vector<int> rightInts(num.begin()+middle+1, num.end());
+        
+//         root->left = sortedArrayToBST(leftInts);
+//         root->right = sortedArrayToBST(rightInts);
+        
+        return SortedtoBST(a,0,a.size()-1);
     }
 };
