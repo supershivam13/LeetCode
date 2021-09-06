bfa7a5358f456249a1acbab38041aa99c97166ea
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-09-06 17:07:32 +0530

    Time: 8 ms (74.57%), Space: 14.9 MB (53.26%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,24 +1,47 @@
+// Time Complexity - O(N + M)
+// Space Complexity - O(N + M)
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
-        if(l1==NULL)
-            return l2;
-        if(l2==NULL)
-            return l1;
-        
-        ListNode* c;
-        
-        if(l1->val < l2->val){
-            c=l1;
-            c->next = mergeTwoLists(l1->next,l2);
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+
+        if (a == NULL)
+            return b;
+        if (b == NULL)
+            return a;
+
+        ListNode* p1=a;
+        ListNode* p2=b;
+        ListNode* dummyNode = new ListNode();
+        ListNode* p3 = dummyNode;
+
+        while(p1!=NULL and p2!=NULL){
+            
+            if(p1->val < p2->val){
+                p3->next=p1;
+                p1=p1->next;
+            }
+            else{
+                p3->next=p2;
+                p2 = p2->next;
+            }
+            p3=p3->next;
         }
-        else{
-            c=l2;
-            c->next = mergeTwoLists(l1,l2->next);
+        
+        while(p1!=NULL){
+            p3->next=p1;
+            p1=p1->next;
+            p3=p3->next;
         }
         
-        return c;
+        while(p2!=NULL){
+            p3->next=p2;
+            p2=p2->next;
+            p3=p3->next;
+        }
+        
+        ListNode* newhead = dummyNode->next;
+        
+        return newhead;
     }
 };
