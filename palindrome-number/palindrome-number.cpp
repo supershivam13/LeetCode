c49273d9966091940984c06a9b7186c7ecb40276
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-11-13 16:49:41 +0530

    Time: 4 ms (94.80%), Space: 5.9 MB (30.97%) - LeetHub

---
+++ palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        int start=0;
+        int end=s.length()-1;
+        while(start<=end){
+            if(s[start]!=s[end])
+                return false;
+            
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+};
