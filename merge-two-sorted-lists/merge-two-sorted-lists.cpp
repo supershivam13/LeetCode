c6414b024b9a8c5276121c257a922218d6f97105
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-11-13 17:57:40 +0530

    Time: 12 ms (36.51%), Space: 14.7 MB (88.52%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,6 +1,3 @@
-// Time Complexity - O(N + M)
-// Space Complexity - O(N + M)
-
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
@@ -10,38 +7,18 @@ public:
         if (b == NULL)
             return a;
 
-        ListNode* p1=a;
-        ListNode* p2=b;
-        ListNode* dummyNode = new ListNode();
-        ListNode* p3 = dummyNode;
+        ListNode* result;
 
-        while(p1!=NULL and p2!=NULL){
-            
-            if(p1->val < p2->val){
-                p3->next=p1;
-                p1=p1->next;
-            }
-            else{
-                p3->next=p2;
-                p2 = p2->next;
-            }
-            p3=p3->next;
+        // SORTING THE LISTS RECURSIVELY
+        if (a->val <= b->val) {
+            result = a;
+            result->next = mergeTwoLists(a->next, b);
         }
-        
-        while(p1!=NULL){
-            p3->next=p1;
-            p1=p1->next;
-            p3=p3->next;
+        else {
+            result = b;
+            result->next = mergeTwoLists(a, b->next);
         }
-        
-        while(p2!=NULL){
-            p3->next=p2;
-            p2=p2->next;
-            p3=p3->next;
-        }
-        
-        ListNode* newhead = dummyNode->next;
-        
-        return newhead;
+
+        return result;
     }
 };
