6e73fc72497a235524ded4f78fbe309347b20dda
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-11-30 17:32:50 +0530

    Time: 20 ms (66.85%), Space: 14.4 MB (42.34%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& a) {
-        
+
         vector<vector<int>> ans;
-        int n=a.size();
-        vector<int> last(2,INT_MAX);
+
+        // n stores the size of the actual given vector
+        int n = a.size();
+        int i = 0, j;
+
+        // inserting a {INT_MAX,INT_MAX} vector for ease
+        vector<int> last(2, INT_MAX);
         a.push_back(last);
-        sort(a.begin(),a.end());
-        int i=0,j;
-        bool b=false;
-        
-        while(i+1<=n){
-        
-            while(i+1<=n and a[i][1]<a[i+1][0]){
-                b=true;
+        sort(a.begin(), a.end());
+
+        // since a.size() is increased by 1 after inserting, so can access upto index n
+        while (i + 1 <= n) {
+
+            // simply inserting all the non-overlapping intervals
+            while (i + 1 <= n and a[i][1] < a[i + 1][0]) {
                 ans.push_back(a[i]);
                 i++;
             }
-            
-            // // if(b){
-            //     i++;
-            //     b=false;
-            // }
 
-            while(i+1<=n and a[i][1]>=a[i+1][0]){
-                
-                j=i+1;
+            // case of merging of two or more intervals
+            while (i + 1 <= n and a[i][1] >= a[i + 1][0]) {
 
-                while(j<n and a[i][1]>=a[j][0]){
-                    a[i][0]=min(a[i][0],a[j][0]);
-                    a[i][1]=max(a[i][1],a[j][1]);
+                j = i + 1;
+
+                // determining the lower and upper bound of the final merged interval
+                while (j<n and a[i][1] >= a[j][0]) {
+                    a[i][0] = min(a[i][0], a[j][0]);
+                    a[i][1] = max(a[i][1], a[j][1]);
                     j++;
                 }
-                
+
                 ans.push_back(a[i]);
-                i=j;
+                i = j;
             }
         }
-    
-    return ans;
+
+        return ans;
     }
 };
