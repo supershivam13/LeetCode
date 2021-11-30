1dd1c0072fb16b7066f060c1f93e73147dc501d8
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-11-30 23:25:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.12%) - LeetHub

--- valid-parentheses/valid-parentheses.cpp
+++ valid-parentheses/valid-parentheses.cpp
@@ -10,30 +10,29 @@ public:
             if(x[i]=='(' or x[i]=='{' or x[i]=='[')
                 s.push(x[i]);
             
-            else if(x[i]==')'){
+            else if(x[i]==')')
                 if(s.empty() or s.top()!='(')
                     return false;
                 else
                     s.pop();
-            }
-            else if(x[i]=='}'){
+            
+            else if(x[i]=='}')
                 if(s.empty() or s.top()!='{')
                     return false;
                 else
                     s.pop();
-            }
-            else if(x[i]==']'){
+            
+            else if(x[i]==']')
                 if(s.empty() or s.top()!='[')
                     return false;
                 else
                     s.pop();
-            }
+            
         }
         
         if(s.size()==0)
             return true;
         else
             return false;
-        
     }
 };
